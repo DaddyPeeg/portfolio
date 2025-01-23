b5f6b2c6132867d155b8fc9b3ae3a7f528fc4212
Zeff <zeff.somera@volatility.com.au>
2025-01-23 13:45:57 +0800

    mobile of skill section

--- src/components/SkillCard/SkillsSlider.tsx
+++ src/components/SkillCard/SkillsSlider.tsx
@@ -10,7 +10,7 @@ const SkillsSlider = ({ chartData }: Props) => {
   const Icon = chartData.icon;
   return (
     <motion.div
-      key={chartData.month} // Add key prop to force remount on chartData change
+      key={chartData.month}
       className="flex justify-center w-full overflow-x-auto gap-4 p-4"
       initial={{
         opacity: 0,
@@ -26,13 +26,15 @@ const SkillsSlider = ({ chartData }: Props) => {
         },
       }}
     >
-      <div className="skill_card text-white shrink-0 w-full max-w-[24rem] h-[26rem]">
+      {/* h-[26rem] */}
+      <div className="skill_card text-white shrink-0 w-full max-w-[24rem] h-[14rem] p-2">
         <div className="skill_card__border" />
-        <span className="bg-gray-100 z-50 absolute p-2 top-3 self-start rounded-full">
-          <Icon className="size-8 rounded-full text-black" />
+        <span className="bg-gray-100 z-50 absolute p-1 md:p-2 md:top-3 self-start rounded-full top-1">
+          <Icon className="size-5 md:size-8 rounded-full text-black" />
         </span>
+        {/* relative border-t border-b border-r border-gray-100 rounded-xl flex text-nowrap w-32 overflow-hidden */}
         <motion.div
-          className="relative border-t border-b border-r border-gray-100 rounded-xl flex text-nowrap w-32 overflow-hidden"
+          className="relative border-t border-b border-r border-gray-100 rounded-xl text-nowrap overflow-hidden shrink-0"
           key={chartData.month}
           initial={{
             width: 0,
@@ -51,48 +53,52 @@ const SkillsSlider = ({ chartData }: Props) => {
             className="absolute w-full h-full bg-red-500"
            
           ></motion.div> */}
-          <h1 className="text-base ml-14 font-semibold">{chartData.month}</h1>
+          <h1 className="text-xs font-semibold md:text-base ml-10 md:ml-14 md:font-semibold ">
+            {chartData.month}
+          </h1>
         </motion.div>
-        <div className="line shrink-0 mt-6 mb-2" />
-        <ul className="space-y-2">
-          {chartData.tech.map((tech, techIndex) => {
-            const Logo = tech.logo;
-            return (
-              <li
-                className="flex relative justify-between"
-                key={`${chartData.month}-${tech.name}`}
-              >
-                <div className="flex items-center">
-                  <Logo />
-                  <p className="text-sm font-bold ml-2 items-center">
-                    {tech.name}
-                  </p>
-                </div>
+        <div className="line shrink-0 my-2 md:mt-6 md:mb-2" />
+        <div className="h-full overflow-y-auto hide_scrollbar">
+          <ul className="space-y-2">
+            {chartData.tech.map((tech, techIndex) => {
+              const Logo = tech.logo;
+              return (
+                <li
+                  className="flex relative justify-between items-center"
+                  key={`${chartData.month}-${tech.name}`}
+                >
+                  <div className="flex items-center">
+                    <Logo />
+                    <p className="md:text-sm text-xs font-thin md:font-bold ml-2 items-center">
+                      {tech.name}
+                    </p>
+                  </div>
 
-                <div className="h-1 hover:glow transition-custom w-32 bg-gray-200 rounded-full mt-2 ">
-                  <motion.div
-                    className="h-full bg-gradient rounded-full "
-                    style={{ width: `${tech.score * 10}%` }}
-                    initial={{
-                      scaleX: 0,
-                      originX: 0,
-                    }}
-                    variants={{
-                      visible: {
-                        scaleX: 1,
-                        transition: {
-                          duration: 1,
-                          delay: 1 + techIndex * 0.2,
+                  <div className="h-1 hover:glow transition-custom w-32 bg-gray-200 rounded-full ">
+                    <motion.div
+                      className="h-full bg-gradient rounded-full "
+                      style={{ width: `${tech.score * 10}%` }}
+                      initial={{
+                        scaleX: 0,
+                        originX: 0,
+                      }}
+                      variants={{
+                        visible: {
+                          scaleX: 1,
+                          transition: {
+                            duration: 1,
+                            delay: 1 + techIndex * 0.2,
+                          },
                         },
-                      },
-                    }}
-                    whileInView={"visible"}
-                  />
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+                      }}
+                      whileInView={"visible"}
+                    />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </motion.div>
   );
